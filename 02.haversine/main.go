f7dbb0f15ccf9ef86b62738fd83b5dc0826f5f26
GiGGioSo <gioele.pettarini@gmail.com>
2025-10-18 23:24:51 +0200

    basic version

--- 02.haversine/main.go
+++ 02.haversine/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"os"
+	"log"
 	"fmt"
 	"flag"
+	"math/rand"
     "GiGGioSo/pcourse/distance/haversine"
 )
+
+var earth_radius float64 = 6372.8
 
 func main() {
 	// CLI flags
 	p_help := flag.Bool("help", false, "Print command usage")
 	p_uniform := flag.Bool("uniform", false, "Use uniform random distribution")
 	p_num := flag.Int("n", 10, "Number of points to generate")
-	p_outpath := flag.String("out", "data", "Number of points to generate")
+	p_outpath := flag.String("out", "data.json", "Number of points to generate")
 	flag.Parse()
 	help := *p_help
 	uniform := *p_uniform
@@ -23,15 +28,35 @@ func main() {
 		return
 	}
 
-    fmt.Println("uniform:", uniform)
-    fmt.Println("num:", num)
-    fmt.Println("outpath:", outpath)
+    log.Println("uniform:", uniform)
+    log.Println("num:", num)
+    log.Println("outpath:", outpath)
 
-    // earth_radius := 6372.8
-    earth_radius := 6372.8
+	outfile, err := os.Create(outpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
 
+	outfile.Write([]byte("{ \"pairs\": [\n"))
     for i := 0; i < num; i++ {
-        hav := haversine.ReferenceHaversine(-100, -100, 100, 100, earth_radius)
-        fmt.Println("result:", hav)
+		x0 := rand.Float64() * 360 - 180
+		x1 := rand.Float64() * 360 - 180
+		y0 := rand.Float64() * 180 - 90
+		y1 := rand.Float64() * 180 - 90
+        haversine.ReferenceHaversine(x0, y0, x1, y1, earth_radius)
+
+		ending_string := " },\n"
+		if i == num - 1 {
+			ending_string = " }\n"
+		}
+
+		row := fmt.Sprint("    ",
+			"{ \"x0\": ", x0, ", \"y0\": ", y0,
+			", \"x1\": ", x1, ", \"y1\": ", y1, ending_string)
+
+        log.Println(row)
+		outfile.WriteString(row)
     }
+	outfile.Write([]byte("]}\n"))
 }
