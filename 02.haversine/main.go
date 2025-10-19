dd06dd41028d94468899b5bc5506d12416b35be0
GiGGioSo <gioele.pettarini@gmail.com>
2025-10-19 15:04:33 +0200

    finished first task

--- 02.haversine/main.go
+++ 02.haversine/main.go
@@ -5,23 +5,43 @@ import (
 	"log"
 	"fmt"
 	"flag"
+	"encoding/binary"
 	"math/rand"
     "GiGGioSo/pcourse/distance/haversine"
 )
 
 var earth_radius float64 = 6372.8
 
+var generator_width, generator_height float64 =
+	rand_range(40, 80), rand_range(20, 40)
+var generator1_x, generator1_y float64 =
+	rand_range(
+		-180 + generator_width/2,
+		180 - generator_width/2),
+	rand_range(
+		-90 + generator_height/2,
+		90 - generator_height/2)
+var generator2_x, generator2_y float64 =
+	rand_range(
+		-180 + generator_width/2,
+		180 - generator_width/2),
+	rand_range(
+		-90 + generator_height/2,
+		90 - generator_height/2)
+
 func main() {
 	// CLI flags
 	p_help := flag.Bool("help", false, "Print command usage")
 	p_uniform := flag.Bool("uniform", false, "Use uniform random distribution")
 	p_num := flag.Int("n", 10, "Number of points to generate")
-	p_outpath := flag.String("out", "data.json", "Number of points to generate")
+	p_outpath := flag.String("out", "data.json", "Path of the output file")
+	p_debugpath := flag.String("debug", "debug.float64", "Path of the debug file")
 	flag.Parse()
 	help := *p_help
 	uniform := *p_uniform
     num := *p_num
     outpath := *p_outpath
+    debugpath := *p_debugpath
     
 	if help {
 		flag.PrintDefaults()
@@ -31,32 +51,73 @@ func main() {
     log.Println("uniform:", uniform)
     log.Println("num:", num)
     log.Println("outpath:", outpath)
+    log.Println("debugpath:", debugpath)
 
 	outfile, err := os.Create(outpath)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil { log.Fatal(err) }
 	defer outfile.Close()
 
-	outfile.Write([]byte("{ \"pairs\": [\n"))
-    for i := 0; i < num; i++ {
-		x0 := rand.Float64() * 360 - 180
-		x1 := rand.Float64() * 360 - 180
-		y0 := rand.Float64() * 180 - 90
-		y1 := rand.Float64() * 180 - 90
-        haversine.ReferenceHaversine(x0, y0, x1, y1, earth_radius)
+	debugfile, err := os.Create(debugpath)
+	if err != nil { log.Fatal(err) }
+	defer debugfile.Close()
 
-		ending_string := " },\n"
+	var average_hav float64 = 0.0
+
+	_, err = outfile.Write([]byte("{ \"pairs\": [\n"))
+	if err != nil { log.Fatal(err) }
+    for i := 0; i < num; i++ {
+		x0, y0, x1, y1 := generate_positions(uniform)
+		hav := haversine.ReferenceHaversine(x0, y0, x1, y1, earth_radius)
+
+		row := fmt.Sprintf("    { \"x0\": %.6f, \"y0\": %.6f, \"x1\": %.6f, \"y1\": %.6f", x0, y0, x1, y1)
 		if i == num - 1 {
-			ending_string = " }\n"
+			row += " }\n"
+		} else {
+			row += " },\n"
 		}
 
-		row := fmt.Sprint("    ",
-			"{ \"x0\": ", x0, ", \"y0\": ", y0,
-			", \"x1\": ", x1, ", \"y1\": ", y1, ending_string)
+		average_hav += (hav - average_hav) / float64(i+1)
 
-        log.Println(row)
-		outfile.WriteString(row)
+		err = binary.Write(debugfile, binary.LittleEndian, hav)
+		if err != nil { log.Fatal(err) }
+
+		_, err = outfile.WriteString(row)
+		if err != nil { log.Fatal(err) }
     }
-	outfile.Write([]byte("]}\n"))
+	_, err = outfile.Write([]byte("]}\n"))
+	if err != nil { log.Fatal(err) }
+
+	err = binary.Write(debugfile, binary.LittleEndian, average_hav)
+	if err != nil { log.Fatal(err) }
+
+	log.Printf("haverage: %.6f\n", average_hav);
 }
+
+func generate_positions(uniform bool) (x0, y0, x1, y1 float64) {
+	if uniform {
+		x0 = rand_range(-180, 180)
+		y0 = rand_range(-90, 90)
+		x1 = rand_range(-180, 180)
+		y1 = rand_range(-90, 90)
+	} else {
+		x0 = rand_range(
+			generator1_x - generator_width / 2,
+			generator1_x + generator_width / 2)
+		y0 = rand_range(
+			generator1_y - generator_height / 2,
+			generator1_y + generator_height / 2)
+		x1 = rand_range(
+			generator2_x - generator_width / 2,
+			generator2_x + generator_width / 2)
+		y1 = rand_range(
+			generator2_y - generator_height / 2,
+			generator2_y + generator_height / 2)
+	}
+	return x0, y0, x1, y1
+}
+
+func rand_range(from, to float64) (value float64) {
+	value = rand.Float64() * (to - from) + from
+	return value
+}
+
